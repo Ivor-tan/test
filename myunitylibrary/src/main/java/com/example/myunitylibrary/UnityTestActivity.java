ace9d1ba17b844365cbc1fc71bc6d2d21a04ba32
Ivor-tan <1336957191@qq.com>
2019-10-25 17:37:41 +0800

    unity library 添加

---
+++ myunitylibrary/src/main/java/com/example/myunitylibrary/UnityTestActivity.java
@@ -0,0 +1,9 @@
+package com.example.myunitylibrary;
+
+import com.unity3d.player.UnityPlayerActivity;
+
+public class UnityTestActivity extends UnityPlayerActivity {
+    public UnityTestActivity() {
+        super();
+    }
+}
