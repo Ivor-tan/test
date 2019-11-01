7a72c9875282fef69c59ddfc4f0532133c65714d
Ivor-tan <1336957191@qq.com>
2019-11-01 17:03:19 +0800

    安卓调用C#方法

--- unity_libs/src/main/java/com/ivor/unity_libs/activity/MainActivity.java
+++ unity_libs/src/main/java/com/ivor/unity_libs/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ivor.unity_libs.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,7 @@ import android.widget.Toast;
 
 import com.ivor.unity_libs.R;
 import com.ivor.unity_libs.R2;
+import com.unity3d.player.UnityPlayerActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,5 +30,6 @@ public class MainActivity extends Activity {
     @OnClick({R2.id.go_scene})
     void OnClick(View view) {
         Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(MainActivity.this, UnitySceneActivity.class));
     }
 }
--- unity_libs/src/main/java/com/ivor/unity_libs/activity/UnitySceneActivity.java
+++ unity_libs/src/main/java/com/ivor/unity_libs/activity/UnitySceneActivity.java
@@ -1,7 +1,54 @@
 package com.ivor.unity_libs.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.ivor.unity_libs.R;
+import com.ivor.unity_libs.R2;
+import com.unity3d.player.UnityPlayer;
 import com.unity3d.player.UnityPlayerActivity;
 
-public class UnitySceneActivity  extends UnityPlayerActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class UnitySceneActivity extends UnityPlayerActivity {
+    @BindView(R2.id.unity_scene)
+    LinearLayout mLlUnityContainer;
+
+    @BindView(R2.id.unity_button)
+    Button unity_button;
+
+    @BindView(R2.id.unity_start_button)
+    Button start_button;
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_unity_scene);
+        ButterKnife.bind(this);
+
+        //将Unity的View添加到布局里
+        View scene = mUnityPlayer.getView();
+        mLlUnityContainer.addView(scene);
+//        UnityPlayer.LoadLevel
+        UnityPlayer.UnitySendMessage("Text_Loading","StartGame","");
+        unity_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(UnitySceneActivity.this, "开始啊", Toast.LENGTH_SHORT).show();
+                UnityPlayer.UnitySendMessage("Slider_Loading","Loading","TankGame");
+            }
+        });
+
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UnityPlayer.UnitySendMessage("Slider_Loading","StartGame","");
+            }
+        });
+
+    }
 }
